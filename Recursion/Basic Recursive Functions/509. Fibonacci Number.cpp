d270dce81b97c291ca65c47bd44b8cffc1446ff2
Ashish Singh <ashishsingh.1899@gmail.com>
2026-01-01 10:56:08 +0530

    509. Fibonacci Number

---
+++ Recursion/Basic Recursive Functions/509. Fibonacci Number.cpp
@@ -0,0 +1,11 @@
+// 509. Fibonacci Number
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+};
